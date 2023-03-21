0ca718245ceef0e55edaf66961e91b1368d59529
temio <temioyelade8@gmail.com>
2023-03-21 22:35:16 +0000

    created DOM element variables

---
+++ assets/js/logic.js
@@ -0,0 +1,17 @@
+var currentQuestionIndex = 0;
+var time = 60;
+var timerId;
+var penalty = 10;
+
+// DOM elements variables
+var questionsA = document.getElementById("questions");
+var timerA = document.getElementById("time");
+var choicesA = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsA = document.getElementById("initials");
+var feedbackA = document.getElementById("feedback");
+var endScreen = document.getElementById("end-screen");
+var startScreen = document.getElementById("start-screen");
+var finalScore = document.getElementById("final-score");
+var questionTitle = document.getElementById("question-title");
