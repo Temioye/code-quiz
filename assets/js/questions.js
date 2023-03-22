6d154cd400d1dd2cfdaa213b33b34ee781a0d921
temio <temioyelade8@gmail.com>
2023-03-22 11:44:18 +0000

    added questions and answers to js

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+var questions = [
+  {
+    questions:
+      "You can pass an anonymous function as an argument to another function?",
+    choices: ["True", "False"],
+    answer: "True",
+  },
+
+  {
+    questions: "Where is the correct place to insert a JavaScript?",
+    choices: [
+      "the <head> section",
+      "Both the <head> section and the <body> section are correct",
+      "The <body> section",
+    ],
+    answer: "Both the <head> section and the <body> section are correct",
+  },
+  {
+    questions:
+      "Which of the following function of String object combines the text of two strings and returns a new string?",
+    choices: ["add()", "merge()", "concat()", "append()"],
+    answer: "concat()",
+  },
+  {
+    questions: "What html tag is NOT included in the HEAD tag?",
+    choices: ["link", "meta", "title", "header"],
+    answer: "header",
+  },
+  {
+    questions: "How can you add a comment in a JavaScript?",
+    choices: [
+      "//This is a comment",
+      "'This is a comment",
+      "<!--This is a comment-->",
+    ],
+    answer: "//This is a comment",
+  },
+];
